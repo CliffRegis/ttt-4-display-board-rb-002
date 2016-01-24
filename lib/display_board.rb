17ce187cc446d2db90bed57ccd8c04b49ccb9a2e
Cliff <cregisexcel@yahoo.com>
2016-01-24 09:00:44 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(arry)
+  print " #{arry[0]} | #{arry[1]} | #{arry[2]} "
+  print "-----------"
+  print " #{arry[3]} | #{arry[4]} | #{arry[5]} "
+  print "-----------"
+  print " #{arry[6]} | #{arry[7]} | #{arry[8]} "
+end
+
